680721ccc6fa5b764ec1746ec7049c25b2ea6d7f
Julia Wigenstedt <juliaemmamatilda@gmail.com>
2020-10-22 16:46:21 +0200

    Added class for GameGraphics

---
+++ src/GameGraphics.java
@@ -0,0 +1,11 @@
+import javax.swing.*;
+
+/**
+ * Created by Julia Wigenstedt
+ * Date: 2020-10-22
+ * Time: 16:45
+ * Project: Inlämning3GUI
+ * Copyright: MIT
+ */
+public class GameGraphics extends JPanel {
+}
