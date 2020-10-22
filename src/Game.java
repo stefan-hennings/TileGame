bc8324dd419e136b40575c76fb4b7441e2d38b41
Julia Wigenstedt <juliaemmamatilda@gmail.com>
2020-10-22 16:53:14 +0200

    Added a constructor for frame and some relevant parameters.

--- src/Game.java
+++ src/Game.java
@@ -2,4 +2,18 @@ import javax.swing.*;
 
 public class Game extends JFrame {
     // TODO: 22-Oct-20 Write code
+    private static ImageIcon icon = new ImageIcon("numberfifteen.png");
+
+    public Game(){
+        setIconImage(icon.getImage());
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setTitle("Världens bästa brickspel, typ");
+        setLocationRelativeTo(null);
+        setVisible(true);
+
+    }
+
+    public static void main(String[] args) {
+        new Game();
+    }
 }
