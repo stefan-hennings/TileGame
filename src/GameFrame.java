6cfc23d601a086f25876dd1b0b2ce153335586a5
Julia Wigenstedt <juliaemmamatilda@gmail.com>
2020-10-27 00:26:42 +0100

    Added new class, GameFrame, to move most of the code from Game

---
+++ src/GameFrame.java
@@ -0,0 +1,11 @@
+import javax.swing.*;
+
+/**
+ * Created by Julia Wigenstedt
+ * Date: 2020-10-27
+ * Time: 00:26
+ * Project: Inlämning3GUI
+ * Copyright: MIT
+ */
+public class GameFrame extends JFrame {
+}
