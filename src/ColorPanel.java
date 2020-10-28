6c2c8056b97a265eb505e7f165066c8e813b60e6
Julia Wigenstedt <juliaemmamatilda@gmail.com>
2020-10-28 14:14:12 +0100

    Added class ColorPanel

---
+++ src/ColorPanel.java
@@ -0,0 +1,11 @@
+import javax.swing.*;
+
+/**
+ * Created by Julia Wigenstedt
+ * Date: 2020-10-28
+ * Time: 14:13
+ * Project: Inlämning3GUI
+ * Copyright: MIT
+ */
+public class ColorPanel extends JPanel {
+}
